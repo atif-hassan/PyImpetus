ab507d747e6467244bfa9c2afa7168b0a1e5849f
Henri Yandell <477715+hyandell@users.noreply.github.com>
2022-08-24 11:02:15 -0700

    Uncommenting the MIT license line

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
          "Programming Language :: Python :: 3",
          "Programming Language :: Python :: 3.6",
          "Programming Language :: Python :: 3.7",
-         #"License :: OSI Approved :: MIT License",
+         "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ]
  )
